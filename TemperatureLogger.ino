61a32073f6de83ca1e9290b5bbf2626a8960e748
markusritschel <git@markusritschel.de>
2022-06-22 14:30:12 +0200

    Initial commit

---
+++ TemperatureLogger.ino
@@ -0,0 +1,14 @@
+/***********************************************************
+ * Add some header documentation here
+************************************************************/
+// Load required libraries
+
+void setup(void)
+{
+// Here comes the "setup" code that will run only once
+}
+
+void loop(void)
+{ 
+// Here comes the code that will run infinitely
+}
