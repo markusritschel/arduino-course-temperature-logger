ba0f9ce82916d17c93ae677f9274188c63478813
markusritschel <git@markusritschel.de>
2022-06-22 14:34:28 +0200

    RTC: print seconds

--- TemperatureLogger.ino
+++ TemperatureLogger.ino
@@ -1,14 +1,34 @@
 /***********************************************************
- * Add some header documentation here
-************************************************************/
+ * Read a DS18B20 temperature sensor
+ * Author: Markus Ritschel
+ * E-Mail: git@markusritschel.de
+ * ************************************************************/
 // Load required libraries
+#include <RTClib.h>
+
+
+// setup instances
+RTC_DS1307 rtc;                  // Setup RTC instance and make available as `rtc`
+
 
 void setup(void)
 {
-// Here comes the "setup" code that will run only once
+  Serial.begin(9600);   // Start Serial communication
+
+  // Initialize RTC
+  if (!rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    Serial.flush();
+    while (1);
+  }
+
+  // automatically set the RTC to the date & time on PC this sketch was compiled
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
 }
 
 void loop(void)
 { 
-// Here comes the code that will run infinitely
+  DateTime now = rtc.now();
+  Serial.println(now.second());
+  delay(1000);
 }
